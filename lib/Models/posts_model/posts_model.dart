2e3da5ed2aabd4fac6e5b5bc2539bebac5da1b58
Sneha2407 <snehabiswas9719@gmail,com>
2023-02-22 23:59:58 +0530

    todos added

---
+++ lib/Models/posts_model/posts_model.dart
@@ -0,0 +1,41 @@
+class PostsModel {
+  int? userId;
+  int? id;
+  String? title;
+  String? body;
+
+  PostsModel({this.userId, this.id, this.title, this.body});
+
+  @override
+  String toString() {
+    return 'PostsModel(userId: $userId, id: $id, title: $title, body: $body)';
+  }
+
+  factory PostsModel.fromJson(Map<String, dynamic> json) => PostsModel(
+        userId: json['userId'] as int?,
+        id: json['id'] as int?,
+        title: json['title'] as String?,
+        body: json['body'] as String?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'userId': userId,
+        'id': id,
+        'title': title,
+        'body': body,
+      };
+
+  PostsModel copyWith({
+    int? userId,
+    int? id,
+    String? title,
+    String? body,
+  }) {
+    return PostsModel(
+      userId: userId ?? this.userId,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      body: body ?? this.body,
+    );
+  }
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("API Tutorial"),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
